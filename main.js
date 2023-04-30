20050f4b2b67fa88c71ac01a8c9e14b64debd2a1
tahatriqui <120963723+tahatriqui@users.noreply.github.com>
2023-04-30 20:44:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const wrapper = document.querySelector(".wrapper") ;
+const wrapper = document.querySelector(".wrapper");
 
-const loginLink = document.querySelector(".login-link") ;
+const loginLink = document.querySelector(".login-link");
 
 const registerLink = document.querySelector(".register-link");
 
@@ -8,20 +8,63 @@ const BtnPopUp = document.querySelector(".btnlogin-popup");
 
 const iconClose = document.querySelector(".button-close");
 
+const input = document.querySelectorAll("input");
+
+const btnCheck = document.querySelector(".btn");
+
+let result = document.querySelector(".result");
+
+const btnCheeck = document.querySelector("#btn");
+
+let r = document.querySelector(".reult");
 
 
-registerLink.addEventListener ('click', ()=> {
+
+
+registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
 
-loginLink.addEventListener ('click', ()=> {
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
 
-BtnPopUp.addEventListener ('click', ()=> {
+BtnPopUp.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener ('click', ()=> {
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
+
+btnCheck.addEventListener('click', () => {
+    let value = input[0].value.trim();
+
+    result.innertext = value.length === 0 ? alert('entrez un email') : "";
+
+})
+
+btnCheck.addEventListener('click', () => {
+    let value = input[1].value.trim();
+
+    result.innertext = value.length === 0 ? alert('entrez un password') : alert("login succes");
+
+})
+
+btnCheeck.addEventListener('click', () => {
+    for (let i=3 ;i<input.length;i++){
+        if( )
+        let value = .value.trim();
+
+    r.innertext = value.length === 0 ? alert('entrez un nom') : "";
+
+    }
+    
+
+})
+
+btnCheeck.addEventListener('click', () => {
+    let value = input[4].value.trim();
+
+    r.innertext = value.length === 0 ? alert('entrez un prenom') : "";
+})
